40a9d5e2c8b3d88da1d34cf4cc9b6a9f77883e8a
nagalil-honaj-hteal3 <elaeth.jonah@gmail.com>
2024-04-01 19:25:03 -0700

    made changes to the posting

--- victim/src/main/java/com/example/demo/Controller.java
+++ victim/src/main/java/com/example/demo/Controller.java
@@ -4,6 +4,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,9 +14,28 @@ public class Controller {
 
     private static final Logger logger = LogManager.getLogger(Controller.class);
 
+    // retrieve from the local directory
     @GetMapping("/")
     public String index() {
         logger.error("get: index");
-        return "Greetings from Spring Boot!";
+        // return "Greetings from Spring Boot!";
+        return 
+                "<h1>Greetings from Spring Boot!</h1>" +
+                "<form action='/' method='post'>" +
+                "<textarea name='message' rows='4' cols='50'></textarea><br><br>" +
+                "<input type='submit' value='Submit'>" + 
+                "</form>";
+    }
+
+    // add this to read the posting from the form
+    @PostMapping("/")
+    @ResponseBody
+    public String handleMessage(@RequestBody String message) {
+        logger.error("Received message from POST: " + message);
+        return 
+                "Message received from POST: " + message + "<br>" +
+                "<form action='/' method='get'>" +
+                "<input type='submit' value='Go back'>" +
+                "</form>";
     }
 }
