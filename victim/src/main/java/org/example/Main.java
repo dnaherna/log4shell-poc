f743e77eb0733964a0e3ecdac5819cba2e2b660a
nagalil-honaj-hteal3 <elaeth.jonah@gmail.com>
2024-04-01 17:21:03 -0700

    updated xml and main.java

--- victim/src/main/java/org/example/Main.java
+++ victim/src/main/java/org/example/Main.java
@@ -3,14 +3,45 @@ package org.example;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class Main {
+// Added --------------
+
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+// --------------------
+
+//added for accessing servlet
+@WebServlet(name = "simpleServlet", value = "/simpleServlet")
+public class Main extends HttpServlet{
 
     private static final Logger logger = LogManager.getLogger(Main.class);
 
-    public static void main(String[] args) {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        String username = req.getParameter("username");
+        
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+
+        logger.error("Hello" + username);
+        // frontend html webpage
+        out.println("<html>");
+            out.println("<body>");
+                out.println("<head><title>Servlet Webpage</title></head>");
+                    out.println("<h1>Hello " + username + "</h1>");
+            out.println("</body>");
+        out.println("</html>");
+    }
+
+    public static void main(String[] args) throws ServletException, IOException {
         System.setProperty("com.sun.jndi.rmi.object.trustURLCodebase", "true");
 
-        String username = args[0];
-        logger.error("Hello: " + username);
+        // doPost();
+        // String username = args[0];
+        // logger.error("Hello: " + username);
+
     }
+
 }
